2bf4299119bab6565be0a504da458a88c4a4456d
Infy <kesarinandankumar2001@gmail.com>
2024-05-26 12:27:18 +0530

    Styling and Restart

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useRef, useEffect } from "react";
 import Webcam from "react-webcam";
-import { Button } from "@mui/material";
+import { Button, Box } from "@mui/material";
 import CameraAltIcon from "@mui/icons-material/CameraAlt";
 import SwitchCameraIcon from "@mui/icons-material/SwitchCamera";
+import RefreshIcon from "@mui/icons-material/Refresh";
 
 const App = () => {
   const [screenshot, setScreenshot] = useState(null);
@@ -16,6 +17,7 @@ const App = () => {
       const videoDevices = devices.filter(
         (device) => device.kind === "videoinput"
       );
+
       // Automatically set the first video device if available
       if (videoDevices.length > 0) {
         setDeviceId(videoDevices[0].deviceId);
@@ -36,6 +38,10 @@ const App = () => {
 
   const handleSwitchCamera = () => {
     setFacingMode((prevMode) => (prevMode === "user" ? "environment" : "user"));
+  };
+
+  const resetWebcam = () => {
+    setScreenshot(null);
   };
 
   return (
@@ -61,24 +67,48 @@ const App = () => {
           />
         )}
       </div>
-      <Button
-        variant="contained"
-        color="primary"
-        startIcon={<CameraAltIcon />}
-        className="mt-2"
-        onClick={captureScreenshot}
-      >
-        Capture Screenshot
-      </Button>
-      <Button
-        variant="contained"
-        color="secondary"
-        startIcon={<SwitchCameraIcon />}
-        className="mt-2"
-        onClick={handleSwitchCamera}
-      >
-        Switch Camera
-      </Button>
+      <Box display="flex" justifyContent="center" mt={2}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={captureScreenshot}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <CameraAltIcon />
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={handleSwitchCamera}
+          sx={{
+            ml: 1,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <SwitchCameraIcon />
+        </Button>
+      </Box>
+      {screenshot && (
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={resetWebcam}
+          sx={{
+            mt: 1,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <RefreshIcon />
+        </Button>
+      )}
     </div>
   );
 };
